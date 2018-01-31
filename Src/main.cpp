0a8f74553a33e408e9f827e06a6d5dd5bba7d2c7
AlexandreMercierAubin <alexandremercieraubin@gmail.com>
2018-01-31 18:14:49 -0500

    test

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,57 +1,24 @@
-#define SDL_MAIN_HANDLED
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
-#include <stdio.h>
-#include <stdlib.h>
-#include "SDL.h"
-
-using namespace std;
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
-			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
-
-			//Wait two seconds
-			SDL_Delay(2000);
-		}
-	}
-
-	//Destroy window
-	SDL_DestroyWindow(window);
-
-	//Quit SDL subsystems
-	SDL_Quit();
-
-
-	return 0;
+    return 0;
 }
