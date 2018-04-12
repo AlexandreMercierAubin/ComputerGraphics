62434d86985e9cf79a57a0b7cc5814c050b2f17e
alexandremercieraubin <alexandremercieraubin@gmail.com>
2018-04-11 22:05:38 -0400

    added parametric surface (copy of curve)

---
+++ Src/ParametricSurfaceObject.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "PrimitiveObject.h"
+#include <functional>
+
+class ParametricCurveObject : public AbstractObject
+{
+public:
+	ParametricCurveObject();
+
+	enum PARAMETRICTYPE
+	{
+		BezierQuadratic,
+		BezierCubic,
+		CatmullRom,
+		Hermite,
+		Bezier
+	};
+	
+	std::vector<PrimitiveObject> lines;
+
+	void setNumLines(int num);
+	void setVertices(std::vector<glm::vec3> &vertices);
+	void setFillColor(glm::vec4 color);
+	void setEdgeColor(glm::vec4 color);
+	void setEdgeSize(int value);
+	void setParametricType(PARAMETRICTYPE in_type);
+	virtual void Create(GLuint &program);
+	virtual void Draw(glm::mat4 &projection, glm::mat4 &view, glm::vec3 &camPos, const vector<Light*>& lights);
+private:
+	PARAMETRICTYPE type;
+	int numLines;
+	static unsigned int factorial(unsigned int n);
+	static  glm::vec3 bezier_quadratic(std::vector<glm::vec3> pt, float t);
+	static  glm::vec3 bezier_cubic(std::vector<glm::vec3> pt, float t);
+	static glm::vec3 hermite(std::vector<glm::vec3> pt, float t);
+	static glm::vec3 bezier(std::vector<glm::vec3> pt, float t);
+	static glm::vec3 catmullRom(std::vector<glm::vec3> pt, float t);
+
+
+	
+	void forNumLines(std::vector<glm::vec3>&vertices, int num, std::function<glm::vec3(vector<glm::vec3>, float)> func);
+
+
+};
